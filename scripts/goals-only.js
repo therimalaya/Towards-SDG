f50ff4d92f22e3189032da29904b17f5e7c9fc76
Raju Rimal <therimalaya@gmail.com>
2019-11-14 17:05:58 +0100

    Init with Pure JavaScript

---
+++ scripts/goals-only.js
@@ -0,0 +1,121 @@
+var goals = {
+  "goal_1": {
+    "description": ["End poverty in all its forms everywhere"],
+    "number": ["1"],
+    "parent": [""],
+    "slug": ["goal_1"],
+    "type": ["goal"]
+  },
+  "goal_10": {
+    "description": ["Reduce inequality within and among countries"],
+    "number": ["10"],
+    "parent": [""],
+    "slug": ["goal_10"],
+    "type": ["goal"]
+  },
+  "goal_11": {
+    "description": ["Make cities and human settlements inclusive, safe, resilient and sustainable"],
+    "number": ["11"],
+    "parent": [""],
+    "slug": ["goal_11"],
+    "type": ["goal"]
+  },
+  "goal_12": {
+    "description": ["Ensure sustainable consumption and production patterns"],
+    "number": ["12"],
+    "parent": [""],
+    "slug": ["goal_12"],
+    "type": ["goal"]
+  },
+  "goal_13": {
+    "description": ["Take urgent action to combat climate change and its impacts"],
+    "number": ["13"],
+    "parent": [""],
+    "slug": ["goal_13"],
+    "type": ["goal"]
+  },
+  "goal_14": {
+    "description": ["Conserve and sustainably use the oceans, seas and marine resources for sustainable development"],
+    "number": ["14"],
+    "parent": [""],
+    "slug": ["goal_14"],
+    "type": ["goal"]
+  },
+  "goal_15": {
+    "description": ["Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification, and halt and reverse land degradation and halt biodiversity loss"],
+    "number": ["15"],
+    "parent": [""],
+    "slug": ["goal_15"],
+    "type": ["goal"]
+  },
+  "goal_16": {
+    "description": ["Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable and inclusive institutions at all levels"],
+    "number": ["16"],
+    "parent": [""],
+    "slug": ["goal_16"],
+    "type": ["goal"]
+  },
+  "goal_17": {
+    "description": ["Strengthen the means of implementation and revitalize the global partnership for sustainable development"],
+    "number": ["17"],
+    "parent": [""],
+    "slug": ["goal_17"],
+    "type": ["goal"]
+  },
+  "goal_2": {
+    "description": ["End hunger, achieve food security and improved nutrition and promote sustainable agriculture"],
+    "number": ["2"],
+    "parent": [""],
+    "slug": ["goal_2"],
+    "type": ["goal"]
+  },
+  "goal_3": {
+    "description": ["Ensure healthy lives and promote well-being for all at all ages"],
+    "number": ["3"],
+    "parent": [""],
+    "slug": ["goal_3"],
+    "type": ["goal"]
+  },
+  "goal_4": {
+    "description": ["Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all"],
+    "number": ["4"],
+    "parent": [""],
+    "slug": ["goal_4"],
+    "type": ["goal"]
+  },
+  "goal_5": {
+    "description": ["Achieve gender equality and empower all women and girls"],
+    "number": ["5"],
+    "parent": [""],
+    "slug": ["goal_5"],
+    "type": ["goal"]
+  },
+  "goal_6": {
+    "description": ["Ensure availability and sustainable management of water and sanitation for all"],
+    "number": ["6"],
+    "parent": [""],
+    "slug": ["goal_6"],
+    "type": ["goal"]
+  },
+  "goal_7": {
+    "description": ["Ensure access to affordable, reliable, sustainable and modern energy for all"],
+    "number": ["7"],
+    "parent": [""],
+    "slug": ["goal_7"],
+    "type": ["goal"]
+  },
+  "goal_8": {
+    "description": ["Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all"],
+    "number": ["8"],
+    "parent": [""],
+    "slug": ["goal_8"],
+    "type": ["goal"]
+  },
+  "goal_9": {
+    "description": ["Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation"],
+    "number": ["9"],
+    "parent": [""],
+    "slug": ["goal_9"],
+    "type": ["goal"]
+  }
+};
