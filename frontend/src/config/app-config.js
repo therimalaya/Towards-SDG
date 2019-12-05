f9aa5beb03ff72c9ec0e3aeaa846f096762a7a18
Raju Rimal <therimalaya@gmail.com>
2019-12-05 12:48:09 +0100

    reformulate state and use semantic for ui

---
+++ frontend/src/config/app-config.js
@@ -0,0 +1,19 @@
+// Configuration of Different Steps for MySteppter
+export const StepConfig = [
+  { 'label': 'Personal Details', 'key': 1 },
+  { 'label': 'Goals', 'key': 2 },
+  { 'label': 'Targets', 'key': 3 },
+  { 'label': 'Summary', 'key': 4 },
+  { 'label': 'Confirmation', 'key': 5 },
+]
+
+export const FacultyConfig = [
+    {key: 'Biovit', value: 'Biovit', text: 'Biosciences'},
+    {key: 'KBM', value: 'KBM', text: 'Chemistry, Biotechnology and Food Science'},
+    {key: 'MINA', value: 'MINA', text: 'Environmental Sciences and Natural Resource Management'},
+    {key: 'Landsam', value: 'Landsam', text: 'Landscape and Society'},
+    {key: 'HH', value: 'HH', text: 'School of Economics and Business'},
+    {key: 'Realtek', value: 'Realtek', text: 'Science and Technology'},
+    {key: 'Vet', value: 'Vet', text: 'Veterinary Medicine'}
+]
+
