71b642b71ec7486318c7a789c617a4c6c14bc5d7
Raju Rimal <therimalaya@gmail.com>
2020-03-26 19:47:26 +0100

    Updates

---
+++ research/precache-manifest.062f1e92ace8f03f8ab79dfbd275acc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ffc0638a76fd1ace73fc6001cde8346",
+    "url": "/Towards-SDG/research/index.html"
+  },
+  {
+    "revision": "9090537870144b70aa71",
+    "url": "/Towards-SDG/research/static/css/main.adb9027e.chunk.css"
+  },
+  {
+    "revision": "e58789ed092b9c06c9e0",
+    "url": "/Towards-SDG/research/static/js/2.0c639f1f.chunk.js"
+  },
+  {
+    "revision": "b9f56fe97c8fa665223f0597d72d2904",
+    "url": "/Towards-SDG/research/static/js/2.0c639f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9090537870144b70aa71",
+    "url": "/Towards-SDG/research/static/js/main.f7688c9a.chunk.js"
+  },
+  {
+    "revision": "1bfd29455d80fc4664e0",
+    "url": "/Towards-SDG/research/static/js/runtime-main.265f8d82.js"
+  }
+]);
