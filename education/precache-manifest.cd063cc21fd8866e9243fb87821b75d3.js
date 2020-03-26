3412adfd9f91854fc4b80d3a5541def2cc895db2
Raju Rimal <therimalaya@gmail.com>
2020-03-26 20:05:30 +0100

    Updates

---
+++ education/precache-manifest.cd063cc21fd8866e9243fb87821b75d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec759fdc5c2de1f0af298991ecc9292",
+    "url": "/Towards-SDG/education/index.html"
+  },
+  {
+    "revision": "42dba444f214a6c106af",
+    "url": "/Towards-SDG/education/static/css/main.adb9027e.chunk.css"
+  },
+  {
+    "revision": "63960f1bff819e50b20a",
+    "url": "/Towards-SDG/education/static/js/2.36ff9425.chunk.js"
+  },
+  {
+    "revision": "b9f56fe97c8fa665223f0597d72d2904",
+    "url": "/Towards-SDG/education/static/js/2.36ff9425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42dba444f214a6c106af",
+    "url": "/Towards-SDG/education/static/js/main.2bc3eab9.chunk.js"
+  },
+  {
+    "revision": "a6541f603e726e7d76d1",
+    "url": "/Towards-SDG/education/static/js/runtime-main.f9e17867.js"
+  }
+]);
