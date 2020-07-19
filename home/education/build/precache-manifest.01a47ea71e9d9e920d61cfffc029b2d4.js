740db310d2fc9466da475df5904b5e96c3c07101
Raju Rimal <therimalaya@gmail.com>
2020-07-20 00:22:33 +0200

    Education site updated

---
+++ home/education/build/precache-manifest.01a47ea71e9d9e920d61cfffc029b2d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d9478766f6eed6c96f72337b628f10",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e5b0e0b566b063c82b3",
+    "url": "./static/css/main.a1e223ae.chunk.css"
+  },
+  {
+    "revision": "7feadd100f29683f2089",
+    "url": "./static/js/2.9960a72c.chunk.js"
+  },
+  {
+    "revision": "e2f543d4bbab70f8a405cd0641de4389",
+    "url": "./static/js/2.9960a72c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5b0e0b566b063c82b3",
+    "url": "./static/js/main.2b707693.chunk.js"
+  },
+  {
+    "revision": "e58ff871944de3834f36",
+    "url": "./static/js/runtime-main.7085325b.js"
+  },
+  {
+    "revision": "5c3f601e5fcc50fc040209b980775819",
+    "url": "./static/media/NMBUwhite.5c3f601e.svg"
+  }
+]);
